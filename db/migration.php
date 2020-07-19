84bb0f2b610cef9f02d5a453c4fb790bc8264576
glintian <lstepsff@gmail.com>
2020-07-19 23:25:15 +0900

    update migration file

--- db/migration.php
+++ db/migration.php
@@ -23,12 +23,32 @@ try{
         exit(); //エラーが起きたときにその後の処理を中断
     }
 
+#pages テーブルの作成
+  $q = "CREATE TABLE pages(
+    id int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+    creatorID int(10) UNSIGNED NOT NULL,
+    title varchar(100) NOT NULL,
+    content text NOT NULL,
+    dateUpdated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    dateAdded timestamp NOT NULL)";
+    $result = $link->exec($q);  
+     
 
-    $q = "CREATE TABLE pages(
-      id int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-      creatorID int(10) UNSIGNED NOT NULL,
-      title varchar(100) NOT NULL,
-      content text NOT NULL,
-      dateUpdated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-      dateAdded timestamp NOT NULL)";
-      $result = $link->exec($q);  
+#commentsテーブルの作成
+  $q = "CREATE TABLE comments(
+    id int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+    creatorID int(10) UNSIGNED NOT NULL,
+    comment text,
+    dateAdded timestamp DEFAULT CURRENT_TIMESTAMP, 
+    pageId int(11))";
+    $result = $link->exec($q);
+
+#usersテーブルの作成
+  $q = "CREATE TABLE comments(
+    id int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+    userType enum('public', 'author', 'admin') NOT NULL,
+    username varchar(30) UNIQUE,
+    email varchar(40) UNIQUE, 
+    pass char(40),
+    dateAdded timestamp DEFAULT CURRENT_TIMESTAMP)";
+  $result = $link->exec($q);
